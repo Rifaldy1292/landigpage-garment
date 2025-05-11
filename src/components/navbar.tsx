cc9a0162e2a5c50daa3976a43157a94cb05755e5
nochamad rifky rifaldy <mochamadrifkyrifaldy@gmail.com>
2025-05-11 17:36:39 +0700

    fixxx

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -31,7 +31,7 @@ export const Navbar = () => {
   };
 
   return (
-    <HeroUINavbar maxWidth="xl" position="sticky " className="fixed z-50">
+    <HeroUINavbar maxWidth="xl" position="sticky" className="fixed z-50">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand className="gap-3 max-w-fit">
           <Link
