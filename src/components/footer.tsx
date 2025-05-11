76d7c0d19b8ff24bb8c8e78b890f25c5ed482115
nochamad rifky rifaldy <mochamadrifkyrifaldy@gmail.com>
2025-05-11 17:15:23 +0700

    fix bug deploy

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import logoPerusahaan from "../assets/logo-perusahaan.jpg";
 import { Link } from "react-router-dom"; // Ganti dengan path logo perusahaan kamu
 const Footer = () => {
--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -2,19 +2,12 @@ import { Link } from "@heroui/link";
 import SecondaryContent from "@/components/secondaryContent";
 import { Navbar } from "@/components/navbar";
 import IndexContent from "@/components/indexContent";
-export default function DefaultLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function DefaultLayout() {
   return (
     <div className="relative flex flex-col h-screen">
       <Navbar />
       <IndexContent />
       <SecondaryContent />
-      {/* <main className="container mx-auto max-w-7xl px-6 flex-grow pt-16">
-        {children}
-      </main> */}
     </div>
   );
 }
