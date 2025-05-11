49cff19657f5ebf16da1d5943f851a5a6f432c75
nochamad rifky rifaldy <mochamadrifkyrifaldy@gmail.com>
2025-05-11 17:18:16 +0700

    fix

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,28 +1,16 @@
-import { Button } from "@heroui/button";
-import { Kbd } from "@heroui/kbd";
 import { Link } from "@heroui/link";
-import { Input } from "@heroui/input";
+
 import {
   Navbar as HeroUINavbar,
   NavbarBrand,
   NavbarContent,
-  NavbarItem,
   NavbarMenuToggle,
   NavbarMenu,
   NavbarMenuItem,
 } from "@heroui/navbar";
-import { link as linkStyles } from "@heroui/theme";
-import clsx from "clsx";
 
 import { siteConfig } from "@/config/site";
-import { ThemeSwitch } from "@/components/theme-switch";
-import {
-  TwitterIcon,
-  GithubIcon,
-  DiscordIcon,
-  HeartFilledIcon,
-  SearchIcon,
-} from "@/components/icons";
+
 import Logo from "../assets/logo-perusahaan.jpg";
 import { useLocation } from "react-router-dom";
 import { motion } from "framer-motion"; // Import motion
